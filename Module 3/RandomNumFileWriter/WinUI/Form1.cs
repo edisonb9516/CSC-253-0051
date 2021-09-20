7ca6a07203c310550e346bbd457efbd30e3a7785
Brandon E <55399973+edisonb9516@users.noreply.github.com>
2021-09-19 22:22:12 -0400

    Uploaded first module 3 folder and proj

---
+++ Module 3/RandomNumFileWriter/WinUI/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WinUI
+{
+    public partial class RandomNumFileWriterForm : Form
+    {
+        public RandomNumFileWriterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateFileButton_Click(object sender, EventArgs e)
+        {
+            int userInput; // hold the user input
+
+            //create an if else to determine if the input was an integer or not
+            if (int.TryParse(userInputTextBox.Text, out userInput))
+            {
+                //use the class to write the file
+                WriteFileClass.WriteFile(userInput);
+
+
+            }
+            //catch anything that isnt an integer and display an error message
+            else 
+            {
+                MessageBox.Show("Error, invalid number. Enter a whole integer.");
+            
+            }
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            //close the application
+            this.Close();
+        }
+    }
+}
---
+++ Module 3/RandomNumFileWriter/WinUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/**
+* 9/19/21
+* CSC 253
+* Brandon Edison
+* This program is going to accept a user integer and use that integer to write to a file with a random number. 
+*/
+namespace WinUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new RandomNumFileWriterForm());
+        }
+    }
+}
